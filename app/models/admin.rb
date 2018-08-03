def3c915e1071a3738a7a9c2547a3ce4fc8527da
Kemal Aydın <kmlcanaydin@gmail.com>
2018-08-03 11:51:22 +0300

    added last gemfile and user model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,4 @@
 class Admin < ApplicationRecord
-  has_one :user, as: :typeable, dependent: :destroy
+  has_one :user, as: :type, dependent: :destroy
   accepts_nested_attributes_for :user
 end
--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,4 +1,4 @@
 class Partner < ApplicationRecord
-  has_one :user, as: :typeable, dependent: :destroy
+  has_one :user, as: :type, dependent: :destroy
   accepts_nested_attributes_for :user
 end
