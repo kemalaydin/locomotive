7958ecc80b956121a71f5461f99ae96d2d02d3e4
Kemal Aydın <kmlcanaydin@gmail.com>
2018-08-03 12:46:36 +0300

    partner page end

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,4 +1,7 @@
 class Partner < ApplicationRecord
   has_one :user, as: :type, dependent: :destroy
   accepts_nested_attributes_for :user
+
+  enum partner_type: [:company, :organization, :person]
+
 end
