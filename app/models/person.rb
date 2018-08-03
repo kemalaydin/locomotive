89b1a62e241ad948ba1bc13a6898e394f0452ae7
Çağatay Yiğit Şahin <cyigitsahin@outlook.com>
2018-08-03 13:06:06 +0300

    Validate the presence of person registration fields

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,7 @@
 class Person < ApplicationRecord
   has_one :user, as: :type, dependent: :destroy
   accepts_nested_attributes_for :user
+  validates_presence_of :name, :surname, :age, :gender, :job, :phone
 
   enum gender: [ :male, :female ]
   enum job: [ :academician, :student, :employer, :employee ]
